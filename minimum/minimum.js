ae6304d64a2a31914c39eaddc421bf6088a09258
Anatorusan <44473563+Anatorusan@users.noreply.github.com>
2023-01-08 13:57:09 +0200

    Added task

--- minimum/minimum.js
+++ minimum/minimum.js
@@ -1,3 +1,9 @@
+// Chapter 2 introduced the standard function Math.min, which returns its smallest argument (see “Return
+// Values” on page 27). We can build something like that now. Write a function min that takes two
+// arguments and returns their minimum.
+
+// Haverbeke, Marijn. Eloquent JavaScript, 3rd Edition (p. 56). No Starch Press. Kindle Edition.
+
 function min(num1, num2) {
     if (num1 > num2) {
         return num2;
