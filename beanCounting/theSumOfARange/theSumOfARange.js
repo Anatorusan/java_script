3ca4f829336b0d3d6cae56e9c71e34819f7f4bc3
Anatorusan <44473563+Anatorusan@users.noreply.github.com>
2023-01-20 15:20:41 +0200

    completed

--- beanCounting/theSumOfARange/theSumOfARange.js
+++ beanCounting/theSumOfARange/theSumOfARange.js
@@ -10,3 +10,49 @@ go up by increments of one, corresponding to the old behavior. The function call
 should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) 
 produces [5, 4, 3, 2].  */
 
+
+// pure range function
+// let range = function(startNum, endNum) {
+//     let numRange = [];
+//     let curNum = startNum;
+//     for (let i = 0; curNum <= endNum; i++) {
+//         numRange [i] = curNum;
+//         curNum += 1;
+//     }
+//     return numRange;
+// }
+
+// console.log(range(1, 10));
+
+// range function with step
+function range(startNum, endNum, step = 1) {
+    let numRange = [];
+    let curNum = startNum;
+    if (step == 0) { 
+        numRange[0] = startNum;
+        return numRange; 
+    }
+    if (step > 0) {
+        for (let i = 0; curNum <= endNum; i++) {
+            numRange [i] = curNum;
+            curNum += step;
+        }
+    } else {
+        for (let i = 0; curNum >= endNum; i++) {
+            numRange [i] = curNum;
+            curNum += step;
+        }
+    }
+    return numRange;
+};
+
+// sum function
+function sum(array) {
+    let sum = 0;
+    for (let entry of array) {
+        sum += entry;
+    }
+    return sum;
+}
+
+console.log(sum(range(10, -20, -3)));
